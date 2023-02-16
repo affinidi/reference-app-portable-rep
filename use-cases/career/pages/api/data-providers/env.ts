4fde6237d123c8a5dcce988c83e74a4da3e7e79a
Ezgi <88712585+ezgi-affinidi@users.noreply.github.com>
2023-02-16 10:40:12 +0100

    feat: added env variables checking (#1)
    * feat: added env variables checking
    
    * feat: fixed comment
    
    * feat: fixed comment
    
    * feat: fixed comment

--- use-cases/career/pages/api/data-providers/env.ts
+++ use-cases/career/pages/api/data-providers/env.ts
@@ -1,8 +1,13 @@
-export const githubClientId = require(process.env.GITHUB_APP_CLIENT_ID)
-export const githubClientSecret = require(process.env.GITHUB_APP_CLIENT_SECRET)
-
-function require<T>(value: T | undefined): T {
-  if (!value)
-    throw new Error('Environment value is missing')
-  return value
+const requiredEnvs = [
+  'GITHUB_APP_CLIENT_ID',
+  'GITHUB_APP_CLIENT_SECRET'
+]
+const missingEnvs = requiredEnvs.filter((name) => !process.env[name])
+if (missingEnvs.length !== 0) {
+  throw new Error(
+     `Required envs are not provided: ${missingEnvs.join(', ')}. Please check README file.`
+  )
 }
+
+export const githubClientId = process.env.GITHUB_APP_CLIENT_ID
+export const githubClientSecret = process.env.GITHUB_APP_CLIENT_SECRET
--- use-cases/career/pages/api/env.ts
+++ use-cases/career/pages/api/env.ts
@@ -1,19 +1,25 @@
 // backend-only envs
 
-export const cloudWalletApiUrl = require(process.env.CLOUD_WALLET_API_URL)
-export const affinidiIamApiUrl = require(process.env.AFFINIDI_IAM_API_URL)
-export const issuanceApiUrl = require(process.env.ISSUANCE_API_URL)
+const requiredEnvs = [
+  'PROJECT_ID',
+  'PROJECT_DID',
+  'API_KEY_HASH'
+]
+const missingEnvs = requiredEnvs.filter((name) => !process.env[name])
+if (missingEnvs.length !== 0) {
+  throw new Error(
+     `Required envs are not provided: ${missingEnvs.join(', ')}. Please check README file.`
+  )
+}
 
-export const projectId = require(process.env.PROJECT_ID)
-export const projectDid = require(process.env.PROJECT_DID)
-export const apiKeyHash = require(process.env.API_KEY_HASH)
+export const cloudWalletApiUrl = process.env.CLOUD_WALLET_API_URL
+export const affinidiIamApiUrl = process.env.AFFINIDI_IAM_API_URL
+export const issuanceApiUrl = process.env.ISSUANCE_API_URL
 
-export const authJwtSecret = require(process.env.AUTH_JWT_SECRET)
+export const projectId = process.env.PROJECT_ID
+export const projectDid = process.env.PROJECT_DID
+export const apiKeyHash = process.env.API_KEY_HASH
+
+export const authJwtSecret = process.env.AUTH_JWT_SECRET
 
 export const logLevel = process.env.LOG_LEVEL || 'info'
-
-function require<T>(value: T | undefined): T {
-  if (!value)
-    throw new Error('Environment value is missing')
-  return value
-}
