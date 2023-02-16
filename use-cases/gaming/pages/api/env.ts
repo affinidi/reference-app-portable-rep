d80bb9e910c1c422e044e75a195429751d8e9d4f
ezgi.a <ezgi.a@affinidi.com>
2023-02-16 09:52:23 +0100

    feat: fixed comment

--- use-cases/gaming/pages/api/env.ts
+++ use-cases/gaming/pages/api/env.ts
@@ -1,21 +1,18 @@
 // backend-only envs
 
-(function requireEnvs() {
-  const names: string[] = [
-    'PROJECT_ID',
-    'PROJECT_DID',
-    'API_KEY_HASH',
-    'GITHUB_APP_CLIENT_ID',
-    'GITHUB_APP_CLIENT_SECRET',
-  ]
-
-  const missingEnvs: string[] = names.filter((name) => !process.env[name])
-  if (missingEnvs.length !== 0) {
-    throw new Error(
-       `Required envs are not provided: ${missingEnvs.join(', ')}. Please check README file.`
-    )
-  }
-}) ()
+const requiredEnvs = [
+  'PROJECT_ID',
+  'PROJECT_DID',
+  'API_KEY_HASH',
+  'GITHUB_APP_CLIENT_ID',
+  'GITHUB_APP_CLIENT_SECRET',
+]
+const missingEnvs = requiredEnvs.filter((name) => !process.env[name])
+if (missingEnvs.length !== 0) {
+  throw new Error(
+     `Required envs are not provided: ${missingEnvs.join(', ')}. Please check README file.`
+  )
+}
 
 export const cloudWalletApiUrl = require(process.env.CLOUD_WALLET_API_URL)
 export const affinidiIamApiUrl = require(process.env.AFFINIDI_IAM_API_URL)
