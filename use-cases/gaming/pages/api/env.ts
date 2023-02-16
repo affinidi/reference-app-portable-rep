be0d72b25b8b07fa3f55086236d88e0d53b0ae5a
ezgi.a <ezgi.a@affinidi.com>
2023-02-16 10:28:09 +0100

    feat: fixed comment

--- use-cases/gaming/pages/api/env.ts
+++ use-cases/gaming/pages/api/env.ts
@@ -12,20 +12,14 @@ if (missingEnvs.length !== 0) {
   )
 }
 
-export const cloudWalletApiUrl = require(process.env.CLOUD_WALLET_API_URL)
-export const affinidiIamApiUrl = require(process.env.AFFINIDI_IAM_API_URL)
-export const issuanceApiUrl = require(process.env.ISSUANCE_API_URL)
+export const cloudWalletApiUrl = process.env.CLOUD_WALLET_API_URL
+export const affinidiIamApiUrl = process.env.AFFINIDI_IAM_API_URL
+export const issuanceApiUrl = process.env.ISSUANCE_API_URL
 
-export const projectId = require(process.env.PROJECT_ID)
-export const projectDid = require(process.env.PROJECT_DID)
-export const apiKeyHash = require(process.env.API_KEY_HASH)
+export const projectId = process.env.PROJECT_ID
+export const projectDid = process.env.PROJECT_DID
+export const apiKeyHash = process.env.API_KEY_HASH
 
-export const authJwtSecret = require(process.env.AUTH_JWT_SECRET)
+export const authJwtSecret = process.env.AUTH_JWT_SECRET
 
 export const logLevel = process.env.LOG_LEVEL || 'info'
-
-function require<T>(value: T | undefined): T {
-  if (!value)
-    throw new Error('Environment value is missing')
-  return value
-}
