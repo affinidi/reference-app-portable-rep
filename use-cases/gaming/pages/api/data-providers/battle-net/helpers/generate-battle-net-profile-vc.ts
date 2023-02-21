da32101d37f42e827fd92e2aa39dec4115d5a1ae
Vitaly Rudenko <vitaly.r@affinidi.com>
2023-02-21 18:19:29 +0200

    fix: use public schema

--- use-cases/gaming/pages/api/data-providers/battle-net/helpers/generate-battle-net-profile-vc.ts
+++ use-cases/gaming/pages/api/data-providers/battle-net/helpers/generate-battle-net-profile-vc.ts
@@ -2,8 +2,8 @@ import { nanoid } from 'nanoid'
 import { BattleNetProfileCredentialSubject } from 'types/data-providers'
 import { VerifiableCredential } from 'types/vc'
 
-const VC_JSON_SCHEMA_URL = 'https://schema.affinidi.com/@did:elem:EiC8fehVnWr2XAXlNSdHWg8fy1s8rMImwgn3YaNZQL--jg/BattleNetProfileV1-3.json'
-const VC_JSON_LD_CONTEXT_URL = 'https://schema.affinidi.com/@did:elem:EiC8fehVnWr2XAXlNSdHWg8fy1s8rMImwgn3YaNZQL--jg/BattleNetProfileV1-3.jsonld'
+const VC_JSON_SCHEMA_URL = 'https://schema.affinidi.com/BattleNetProfileV1-0.json'
+const VC_JSON_LD_CONTEXT_URL = 'https://schema.affinidi.com/BattleNetProfileV1-0.jsonld'
 
 export function generateBattleNetProfileVc(holderDid: string, credentialSubject: BattleNetProfileCredentialSubject): VerifiableCredential {
   return {
